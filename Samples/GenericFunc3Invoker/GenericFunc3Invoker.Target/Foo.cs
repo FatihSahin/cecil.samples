f31c33cd4e908d9e9abc256a76677966f8522ca3
FatihSahin <fatihsahin72@gmail.com>
2018-05-09 01:02:19 +0300

    sample added and set build for fw 4.5.2

---
+++ Samples/GenericFunc3Invoker/GenericFunc3Invoker.Target/Foo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericFunc3Invoker.Target
+{
+    public class Foo
+    {
+        public TRes GetFoo<TArg1, TArg2, TRes>(TArg1 req1, TArg2 req2, string arg3) where TRes : new()
+        {
+            return new TRes();
+        }
+
+        //WHAT I am expecting to weave foo like below (example with bar)
+
+        public TRes GetBar<TArg1, TArg2, TRes>(TArg1 req1, TArg2 req2, string arg3) where TRes : new()
+        {
+            Invoker<TArg1, TArg2, string, TRes> invoker = new Invoker<TArg1, TArg2, string, TRes>();
+            return invoker.Invoke(GetBar_Original<TArg1, TArg2, TRes>, req1, req2, arg3);
+        }
+
+        public TRes GetBar_Original<TArg1, TArg2, TRes>(TArg1 req1, TArg2 req2, string arg3) where TRes : new()
+        {
+            Console.WriteLine("Do some get foo stuff");
+            return new TRes();
+        }
+    }
+}
---
+++ Samples/GenericFunc3Invoker/GenericFunc3Invoker.Target/Invoker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericFunc3Invoker.Target
+{
+    public class Invoker<TArg1, TArg2, TArg3, TRes>
+    {
+        public TRes Invoke(Func<TArg1, TArg2, TArg3, TRes> myFunc, params object[] args)
+        {
+            Console.WriteLine("Do some pre call stuff");
+            TRes result = (TRes)myFunc.Method.Invoke(myFunc.Target, args);
+            Console.WriteLine("Do some post call stuff");
+            return result;
+        }
+    }
+}
